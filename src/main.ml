22ad2ec356e9f7e6d41b1b082b62ac83b7f5f459
Johannes Kanig <johannes@kanig.org>
2013-08-07 15:03:10 +0200

    some changes

--- src/main.ml
+++ src/main.ml
@@ -2,5 +2,8 @@ open Programs
 
 let _ =
   print_endline "hello";
-  Webapi.test ()
+  let x = Webapi.eval "1" (Array.map Int64.of_int [|12;13|]) in
+  for i = 0 to Array.length x - 1 do
+    print_endline (Int64.to_string x.(i))
+  done
 
--- src/webapi.ml
+++ src/webapi.ml
@@ -1,14 +1,16 @@
 open Unix
 
+type problem_id = string
+
 let base_url = "icfpc2013.cloudapp.net"
-let host_name = "toto"
+let host_name = "my host"
 
 let token =
   let fd = open_in "secret.txt" in
   let s = input_line fd in
   s ^ "vpsH1H"
 
-let path = "/train?auth=" ^ token
+let path request = "/" ^ request ^ "?auth=" ^ token
 
 let make_addr host_name port =
   let host_addr =
@@ -23,22 +25,50 @@ let run_client f addr =
   let sock = socket PF_INET SOCK_STREAM 0 in
   connect sock addr;
   f sock
-let test () =
+
+let connect request f =
   let addr = make_addr base_url 80 in
   run_client (fun sock ->
     let in_ch = in_channel_of_descr sock in
     let out_ch = out_channel_of_descr sock in
-    output_string out_ch ("GET "^path^" HTTP/1.1\r\n");
+    output_string out_ch ("POST "^path request^" HTTP/1.1\r\n");
     output_string out_ch ("Host: "^host_name^"\r\n");
-    output_string out_ch "User-Agent: mon programme\r\n";
+    output_string out_ch "User-Agent: dummy\r\n";
     output_string out_ch "Connection: close\r\n";
+    output_string out_ch "Content_Type: application/json\r\n";
     output_string out_ch "\r\n";
     output_string out_ch "";
+    f out_ch;
     flush out_ch;
+    let result = ref "" in
     begin try while true do
-       print_endline (input_line in_ch)
+      result := !result ^ (input_line in_ch);
     done
     with End_of_file -> ()
     end;
-    close sock
+    close sock;
+    !result
   ) addr
+
+let num_to_hex_string x = assert false
+
+let print_as_json_dict ch ar =
+  output_string ch "[";
+  for i = 0 to Array.length ar -1 do
+    if i <> 0 then output_string ch ",";
+    output_string ch "\"";
+    output_string ch (num_to_hex_string ar.(i));
+    output_string ch "\"\r\n";
+  done;
+  output_string ch "]"
+
+let eval prog_id input =
+  let x =
+  connect "eval" (fun ch ->
+    output_string ch "{";
+    output_string ch "program: \"(lambda (x_38661) (not x_38661))\"";
+    output_string ch "arguments: ";
+    print_as_json_dict ch input;
+    output_string ch "} ";) in
+  print_endline x;
+  [| |]
