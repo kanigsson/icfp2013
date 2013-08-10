327eef473eb6f55ccd37681472712c9249f1ecd1
Louis Mandel <louis.mandel@lri.fr>
2013-08-11 01:46:19 +0200

    Benchs.

--- src/bench.ml
+++ src/bench.ml
@@ -22,7 +22,7 @@ let generation size fold_kind n =
     n
 
 let rec find_programs input output gen n acc nb_try =
-  if 0 < n && nb_try < 100_000_000 then
+  if 0 < n && nb_try < 10_000_000 then
     let p = gen () in
     if Programs.validates_constraints input output p then
       find_programs input output gen (n - 1) (p :: acc) (nb_try + 1)
@@ -35,6 +35,7 @@ let find_programs size fold_kind n =
   let start_time = Sys.time () in
   let generate = Program_generator.make unops binops fold_kind size in
   let p = generate () in
+  Format.printf "p = %a@." Programs.print_program p;
   let input = Arguments.int_64_arguments in
   let out = Array.map (Programs.eval p) input in
   let sols, nb_try = find_programs input out generate n [] 0 in
