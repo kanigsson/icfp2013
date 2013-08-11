d652c2faea2634815b064ff30526923420307b15
Louis Mandel <louis.mandel@lri.fr>
2013-08-11 12:06:41 +0200

    Guess_response.

--- src/json.ml
+++ src/json.ml
@@ -10,6 +10,12 @@ let find_assoc x l =
     Format.eprintf "Json.find_assoc %s" x;
     assert false
 
+let int64_of_json x =
+  match x with
+  | `String v | `Intlit v -> Int64.of_string v
+  | `Int v -> Int64.of_int v
+  | _ -> error x
+
 type eval_response =
   | Eval_ok of int64 array
   | Eval_error of string
@@ -21,15 +27,7 @@ let eval_response_of_string s =
       | `String "ok" ->
           begin match find_assoc "outputs" l with
           | `List l ->
-              let l =
-                List.map
-                  (fun x ->
-                    match x with
-                    | `String v | `Intlit v -> Int64.of_string v
-                    | `Int v -> Int64.of_int v
-                    | _ -> error x)
-                  l
-              in
+              let l = List.map int64_of_json l in
               Eval_ok (Array.of_list l)
           | x -> error x
           end
@@ -41,3 +39,33 @@ let eval_response_of_string s =
       | x -> error x
       end
   | x -> error x
+
+
+type guess_response =
+  | Guess_win
+  | Guess_mismatch of int64 * int64 * int64
+  | Guess_error of string
+
+let guess_response s =
+  match from_string s with
+  | `Assoc l ->
+      begin match find_assoc "status" l with
+      | `String "ok" -> Guess_win
+      | `String "mismatch" ->
+          begin match find_assoc "values" l with
+          | `List l ->
+              begin match List.map int64_of_json l with
+              | [v1; v2; v3] -> Guess_mismatch (v1, v2, v3)
+              | _ -> error (from_string s)
+              end
+          | x -> error x
+          end
+      | `String "error" ->
+          begin match find_assoc "message" l with
+          | `String msg -> Guess_error msg
+          | x -> Guess_error (pretty_to_string x)
+          end
+      | x -> error x
+      end
+  | x -> error x
+
